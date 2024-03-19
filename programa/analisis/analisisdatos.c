ed6f06f4df530c727e4736cb49e4061804e1e808
Mynell2022 <jmyerschaves@gmail.com>
2024-03-18 22:17:05 -0600

    Docu interna

--- programa/analisis/analisisdatos.c
+++ programa/analisis/analisisdatos.c
@@ -1,5 +1,12 @@
 #include "analisisdatos.h"
 
+/// @brief Metodo que analiza datos y los almacena en una lista
+/// @param region Region de referencia para el analisis
+/// @param fechaInicio fecha de inicio del rango de fechas aceptable
+/// @param fechaFin fecha final del rango de fechas aceptable
+/// @param dato Tipo de dato que se va a guardar
+/// @param index Tamano de la lista
+/// @return Lista con los datos acumulados
 float* AnalizarDatos(char* region, time_t fechaInicio, time_t fechaFin, int dato, int* index){
     float* valores= (float*)malloc(tamanoClimas*sizeof(float));
     for (int i = 0; i < tamanoClimas; i++) {
@@ -29,12 +36,22 @@ float* AnalizarDatos(char* region, time_t fechaInicio, time_t fechaFin, int dato
     }
     return valores;
 }
+
+/// @brief Metodo que analiza datos y da la media de un conjunto de datos
+/// @param datos Datos a analizar
+/// @param tam tamano de la lista
+/// @return La media generada
 float ObtenerMedia(float* datos,int tam){
     float suma = 0;
     for (int i = 0; i < tam; i++)
         suma += datos[i];
     return suma / tam;
 }
+
+/// @brief Metodo que analiza datos y da la Desviacion estandar de un conjunto de datos
+/// @param datos Datos a analizar
+/// @param tam tamano de la lista
+/// @return La desviacion generada
 float ObtenerDesviacionEstandar(float* datos,int tam){
     float media= ObtenerMedia(datos,tam),varianza=0;
     for (int i = 0; i < tam; i++)
@@ -42,10 +59,18 @@ float ObtenerDesviacionEstandar(float* datos,int tam){
     return sqrt(varianza/tam);
 }
 
+/// @brief Comparador auxiliar para el qsort, muestra quien es mayor
+/// @param a Un dato de la lista
+/// @param b Un dato de la lista
+/// @return Un numero entre 1 y -1
 int comparar(const void *a, const void *b) {
     return (int)(*(float *)a - *(float *)b);
 }
 
+/// @brief Metodo que analiza datos y da la moda de un conjunto de datos
+/// @param datos Datos a analizar
+/// @param tam tamano de la lista
+/// @return La moda generada
 float ObtenerModa(float* datos,int tam){
     int maxFrecuencia=0,moda=-1,actual=0,valor=datos[0];
     qsort(datos,tam,sizeof(float),comparar);
@@ -63,6 +88,11 @@ float ObtenerModa(float* datos,int tam){
         moda=valor;
     }return moda;
 }
+
+/// @brief Metodo que analiza datos y da la mediana de un conjunto de datos
+/// @param datos Datos a analizar
+/// @param tam tamano de la lista
+/// @return La mediana generada
 float ObtenerMediana(float* datos,int tam){
     qsort(datos, tam, sizeof(int), comparar);
     if (tam % 2 == 0) {
